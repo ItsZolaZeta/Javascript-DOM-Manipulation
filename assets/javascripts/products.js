5950596cae234cc9ed244f6f82908de316b2ec75
Tatiana Tuor <tatianatuor@gmail.com>
2021-08-03 13:35:16 +0200

    Completed Socks and sunglasses section

--- assets/javascripts/products.js
+++ assets/javascripts/products.js
@@ -1,5 +1,5 @@
 
-// -------------------------------------------- Hats --------------------------------------------
+// -------------------------------------------- Hats -------------------------------------------- //
 
 let hat = {
   name: "hat1",
@@ -14,14 +14,14 @@ let hat = {
 
 //console.log(hat.toString());
 
-function Hat(name, price, color, imageHref) {
+function Accessory(name, price, color, imageHref) {
   this.name = name;
   this.price = price;
   this.color = color;
   this.imageHref = imageHref;
 }
 
-Hat.prototype.toString = function() {
+Accessory.prototype.toString = function() {
   let text = this.name + ', color: ' + this.color + ', price: ' + this.price + ', image: ' + this.imageHref;
   return text;
 }
@@ -30,49 +30,56 @@ Hat.prototype.toString = function() {
 // console.log(testHat.toString());
 
 let hatArray = [
-  new Hat("Baseball cap", 11.99, "red", "./assets/images/red/hats/1.png"),
-  new Hat("Baseball cap", 11.99, "blue", "./assets/images/blue/hats/1.png"),
-  new Hat("Baseball cap", 11.99, "yellow", "./assets/images/yellow/hats/1.png"),
-  new Hat("Baseball cap", 11.99, "green", "./assets/images/green/hats/1.png"),
-  new Hat("Beanie", 17.99, "red", "./assets/images/red/hats/2.png"),
-  new Hat("Beanie", 17.99, "blue", "./assets/images/blue/hats/2.png"),
-  new Hat("Beanie", 17.99, "green", "./assets/images/green/hats/2.png"),
-  new Hat("Straw hat", 10.99, "yellow", "./assets/images/yellow/hats/3.png"),
-  new Hat("Straw hat", 10.99, "blue", "./assets/images/blue/hats/3.png"),
-  new Hat("Trilby", 10.99, "red", "./assets/images/red/hats/4.png"),
-  new Hat("Trilby", 10.99, "blue", "./assets/images/blue/hats/4.png"),
-  new Hat("Trilby", 10.99, "yellow", "./assets/images/yellow/hats/4.png"),
+  new Accessory("Baseball cap", 11.99, "red", "./assets/images/red/hats/1.png"),
+  new Accessory("Baseball cap", 11.99, "blue", "./assets/images/blue/hats/1.png"),
+  new Accessory("Baseball cap", 11.99, "yellow", "./assets/images/yellow/hats/1.png"),
+  new Accessory("Baseball cap", 11.99, "green", "./assets/images/green/hats/1.png"),
+  new Accessory("Beanie", 17.99, "red", "./assets/images/red/hats/2.png"),
+  new Accessory("Beanie", 17.99, "blue", "./assets/images/blue/hats/2.png"),
+  new Accessory("Beanie", 17.99, "green", "./assets/images/green/hats/2.png"),
+  new Accessory("Straw hat", 10.99, "yellow", "./assets/images/yellow/hats/3.png"),
+  new Accessory("Straw hat", 10.99, "blue", "./assets/images/blue/hats/3.png"),
+  new Accessory("Trilby", 10.99, "red", "./assets/images/red/hats/4.png"),
+  new Accessory("Trilby", 10.99, "blue", "./assets/images/blue/hats/4.png"),
+  new Accessory("Trilby", 10.99, "yellow", "./assets/images/yellow/hats/4.png"),
 ]
 
 function displayHat(hat) {
   let products = document.getElementById("products");
-  //console.log(products);
+
   let component = document.createElement('div');
   component.setAttribute('class', 'accessory col-sm-4');
+
   let card = document.createElement('div');
   card.setAttribute('class', 'card my-3');
+
   let currency = document.createElement('div');
   currency.setAttribute('class', 'currency btn btn-light disabled');
   currency.textContent = hat.price;
+
   let image = document.createElement('img');
   image.setAttribute('class', 'card-img-top');
   image.setAttribute('src', hat.imageHref);
   image.setAttribute('alt', "Image of " + hat.name);
+
   let card_body = document.createElement('div');
   card_body.setAttribute('class', 'card-body text-center');
+
   let card_title = document.createElement('h5');
   card_title.setAttribute('class', 'card-title');
   card_title.textContent = hat.name;
+
   let card_text = document.createElement('p');
   card_text.setAttribute('class', 'card-text');
   card_text.textContent = "Color: ";
+
   let em = document.createElement('em');
   em.textContent = hat.color;
   card_text.appendChild(em);
+
   let button = document.createElement('button');
   button.setAttribute('class', 'btn btn-outline-primary');
   button.textContent = "Add to wishlist!";
-
 
   card_body.appendChild(card_title);
   card_body.appendChild(card_text);
@@ -96,4 +103,107 @@ function renderHats(hats) {
 
 renderHats(hatArray);
 
-// -------------------------------------------- Filter by color --------------------------------------------
+// -------------------------------------------- Filter by color -------------------------------------------- //
+
+const highlightSelectedFilter = (e) => {
+  let buttons = document.querySelector('.btn-group');
+
+  for(let i = 0; i<buttons.children.length; i++) {
+    if(buttons.children[i].getAttribute('class').includes('active')){
+      buttons.children[i].classList.remove('active');
+    }
+  }
+
+  e.target.classList.add('active');
+}
+
+let buttons = document.querySelector('.btn-group');
+for(let i = 0; i<buttons.children.length; i++) {
+  buttons.children[i].addEventListener('click', highlightSelectedFilter)
+}
+
+function addColorClass(accessoriesArray) {
+  let products = document.getElementById("products");
+  for(let i = 0; i<products.children.length; i++) {
+    products.children[i].classList.add(accessoriesArray[i].color);
+  }
+} 
+addColorClass(hatArray);
+
+function filterHatsByColor(e) {
+  let products = document.getElementById("products");
+  for(let i = 0; i<products.children.length; i++) {
+    products.children[i].style.display = "none";
+  }
+
+  for(let i = 0; i<products.children.length; i++) {
+    if (products.children[i].classList.contains(e.target.textContent.toLowerCase())) {
+      products.children[i].style.display = "block";
+    } 
+  } 
+
+  if(e.target.textContent == 'All'){
+    for(let i = 0; i<products.children.length; i++) {
+      products.children[i].style.display = "block";
+    }
+  }
+}
+
+for(let i = 0; i<buttons.children.length; i++) {
+  buttons.children[i].addEventListener('click', filterHatsByColor);
+}
+
+let allButton = document.createElement('button');
+allButton.setAttribute('type', 'button');
+allButton.setAttribute('class', 'btn btn-outline-secondary');
+allButton.textContent = 'All';
+buttons.appendChild(allButton);
+
+allButton.addEventListener('click', filterHatsByColor);
+
+// -------------------------------------------- Socks and sunglasses -------------------------------------------- //
+
+let navbar = document.querySelectorAll('.nav-link');
+
+for(let i = 0; i<navbar.length; i++) {
+  if(navbar[i].textContent != 'Hats') {
+    navbar[i].addEventListener('click', loadRemoteAccessories);
+  }
+  navbar[i].addEventListener('click', loadHats);
+}
+
+function loadRemoteAccessories(e) {
+  let search = e.target.textContent.toLowerCase();
+  let data;
+  let accessories = [];
+
+  let request = new XMLHttpRequest();
+  request.open('GET', search + '.json'); 
+  request.onload = () => {
+    data = JSON.parse(request.responseText);
+    console.log(data.length);
+
+    //clear current displayed accessories
+    let products = document.getElementById("products");
+    for(let i = 0; i<products.children.length; i++) {
+      products.children[i].style.display = "none";
+    }
+
+    //create Accessory objects
+    for(let i = 0; i<data.length; i++) {
+      let newItem = new Accessory(data[i].name, data[i].price, data[i].color, data[i].imageHref);
+      accessories.push(newItem);
+    }
+
+    renderHats(accessories);
+    addColorClass(accessories);
+  }
+  request.send(); 
+}
+
+function loadHats(e) {
+  renderHats(hatArray);
+  addColorClass(hatArray);
+}
+
+// -------------------------------------------- Gloves -------------------------------------------- //
